1fb7e65134ca9f5249aa190d2d0a5b11d4ab450c
Joseph Bruno <brunojoey12@gmail.com>
2020-03-19 17:19:52 -0500

    Installed dependencies.

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -10,3 +10,15 @@ var PORT = process.env.PORT || 8080;
 // Express Handles Data Parsing
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
