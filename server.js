923ab6c476fc3276a0397dc1dd7a88ec38ab9293
Joseph Bruno <brunojoey12@gmail.com>
2020-03-20 20:56:42 -0500

    Rearranged some files to have it be in the root of the repo.

--- server.js
+++ server.js
@@ -8,24 +8,30 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Requires the DB Json file
-const dbFile = require("../../db/db.json");
+const dbFile = require("./Develop/db/db.json");
+
+// Sets static app to the public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Express Handles Data Parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "../../public/notes.html"));
+    res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
 });
 
 // GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 app.get("/api/notes", function (req, res) {
     fs.readFile(dbFile, "utf8", function (err, data) {
         if (err) {
-            console.log("Something terrible occured...");
+            console.log("Something terrible occurred...");
         } else {
-            res.json(data);
+            res.json(dbFile);
         };
     });
 });
@@ -34,15 +40,8 @@ app.get("/api/notes", function (req, res) {
 // add it to the `db.json` file
 // then return the new note to the client.
 app.post("/api/notes", function (req, res) {
-    const newNote = req.body;
-    newNote.id = newNote.title.replace(/\s+/g, "");
-    console.log(newNote);
-
-    const prvsDBFile = fs.readFile(dbFile, "utf8");
-    console.log(prvsDBFile);
-    let jsonPrvsDB = json.parse(prvsDBFile);
-    console(jsonPrvsDB);
-
+    dbFile.push(req.body);
+    res.json("Something wonderful has occurred...!");
 });
 
 
@@ -52,16 +51,19 @@ app.post("/api/notes", function (req, res) {
 // In order to delete a note, you'll need to read all notes from the `db.json` file, 
 // remove the note with the given `id` property
 // then rewrite the notes to the `db.json` file.
-app.delete("/api/notes", function (req, res) {
-    const deleteNote = req.body;
+app.delete("/api/notes/:id", function (req, res) {
+    var id = req.params.id;
 
+    for (var i = 0; i < dbFile.length; i++) {
+        if (id === dbFile[i].id) {
+          dbFile.splice(i, 1);
+        }
+      };
+      res.json(dbFile);
 })
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
 
-app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../../public/index.html"));
-});
 
